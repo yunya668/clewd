74bbbf0bf8375dcc8455e373bbcc937e27ad9a09
yunya668 <152093056+yunya668@users.noreply.github.com>
2024-01-01 00:46:27 +0000

    Update 2 files

---
+++ config.js
@@ -0,0 +1,63 @@
+/*
+* https://rentry.org/teralomaniac_clewd
+* https://github.com/teralomaniac/clewd
+*/
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+    "Cookie": "SET YOUR COOKIE HERE",
+    "CookieArray": [],
+    "Cookiecounter": 3,
+    "CookieIndex": 0,
+    "ProxyPassword": "",
+    "Ip": "127.0.0.1",
+    "Port": 8444,
+    "localtunnel": false,
+    "BufferSize": 1,
+    "SystemInterval": 3,
+    "rProxy": "https://claude.ai",
+    "api_rProxy": "",
+    "padtxt_placeholder": "",
+    "PromptExperimentFirst": "",
+    "PromptExperimentNext": "",
+    "PersonalityFormat": "{{char}}'s personality: {{personality}}",
+    "ScenarioFormat": "Dialogue scenario: {{scenario}}",
+    "Settings": {
+        "RenewAlways": true,
+        "RetryRegenerate": false,
+        "PromptExperiments": true,
+        "SystemExperiments": true,
+        "PreventImperson": false,
+        "AllSamples": false,
+        "NoSamples": false,
+        "StripAssistant": false,
+        "StripHuman": false,
+        "PassParams": false,
+        "ClearFlags": true,
+        "PreserveChats": false,
+        "LogMessages": true,
+        "FullColon": true,
+        "padtxt": 15000,
+        "xmlPlot": true,
+        "Superfetch": true
+    }
+}
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
